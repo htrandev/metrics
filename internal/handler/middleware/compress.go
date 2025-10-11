dd5dfe17757ebc59b8377b300008df23efde2d75
Hieu Tran <h.tran@adspend.ru>
2025-10-11 19:52:10 +0300

    fix: middleware

--- internal/handler/middleware/compress.go
+++ internal/handler/middleware/compress.go
@@ -88,18 +88,18 @@ func Compress() func(next http.Handler) http.Handler {
 					ow = cw
 					defer cw.Close()
 				}
+			}
 
-				contentEncoding := r.Header.Get("Content-Encoding")
-				sendsGzip := strings.Contains(contentEncoding, "gzip")
-				if sendsGzip {
-					cr, err := newCompressReader(r.Body)
-					if err != nil {
-						w.WriteHeader(http.StatusInternalServerError)
-						return
-					}
-					r.Body = cr
-					defer cr.Close()
+			contentEncoding := r.Header.Get("Content-Encoding")
+			sendsGzip := strings.Contains(contentEncoding, "gzip")
+			if sendsGzip {
+				cr, err := newCompressReader(r.Body)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					return
 				}
+				r.Body = cr
+				defer cr.Close()
 			}
 
 			next.ServeHTTP(ow, r)
